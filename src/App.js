dbf39101e360a36f24e2363abd4c50e63de41699
Ofallbrains <ofallbrains@gmail.com>
2025-08-26 14:19:35 +0200

    React redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css"
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className='App'>
-      Hello world
+      <>
+        <Profile />
+        <Login/>
+      </>
     </div>
   );
 }
