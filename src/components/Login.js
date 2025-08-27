ad695b58cbf4ceadc381c265185f0be4262edd54
Ofallbrains <ofallbrains>
2025-08-27 11:03:34 +0200

    React redux

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../features/user'
+import { logout } from '../features/user'
 
 function Login() {
+
+  const dispatch = useDispatch()
   return (
       <div>
-         <button>Login</button>
+      <button onClick={() =>
+      { dispatch(login({ name: 'Pedro', age: 20, email: "pedro@gmail.com" })) }}
+      >
+        Login
+      </button>
+
+      <button onClick={() =>
+      { dispatch(logout())}}>
+        Logout
+      </button>
     </div>
   )
 }
